8844e53a8853e2d38800206ad800d5a428cf7f47
anyashuka <anya.schukin@gmail.com>
2018-06-28 17:51:21 +0200

    this function moves ants to end

---
+++ move_ants.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move_ants.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschukin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/28 12:01:26 by aschukin          #+#    #+#             */
+/*   Updated: 2018/06/28 12:01:28 by aschukin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+#include <stdio.h>
+
+void    move_ants(t_lem_in *lem_in)
+{
+    t_room  *tmp;
+    
+    while (1)
+    {
+        tmp = lem_in->end;
+        if (tmp->ant_here != NULL && tmp->ant_here->next == NULL)
+            return ;
+        while (tmp)
+        {
+            if (tmp->path_prev && tmp->path_prev->ant_here && !tmp->ant_here)
+            {
+                tmp->ant_here = tmp->path_prev->ant_here;
+                printf("L%ld-%s ", tmp->ant_here->ant_n, tmp->name);
+            }
+            else if (tmp->ant_here)
+            {
+                tmp->ant_here = tmp->ant_here->next;
+                if (tmp != lem_in->start && tmp->ant_here)
+                    printf("L%ld-%s ", tmp->ant_here->ant_n, tmp->name);
+            }
+            tmp = tmp->path_prev;
+        }
+        printf("\n");
+    }
+}
